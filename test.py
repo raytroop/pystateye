0517c1ccb68755782ab2b033b1cf612dbe29791f
raytroop <raytroop@gmail.com>
2023-09-23 17:42:01 +0800

    remove space

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ from statistical_eye import statistical_eye
 with open('channel_pulse_response_test.csv') as file_name:
     pulse_response = np.loadtxt(file_name, delimiter=",")
 
-results = statistical_eye(pulse_response=pulse_response, 
-                                        samples_per_symbol=128, 
-                                        A_window_multiplier=1.5, 
-                                        M=4, 
-                                        sample_size=16, 
+results = statistical_eye(pulse_response=pulse_response,
+                                        samples_per_symbol=128,
+                                        A_window_multiplier=1.5,
+                                        M=4,
+                                        sample_size=16,
                                         target_BER=2.4e-4,
-                                        plot=True, 
-                                        noise_flag=False, 
+                                        plot=True,
+                                        noise_flag=False,
                                         jitter_flag=False,
                                         upsampling=1)
